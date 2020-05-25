ddb1f1b1b881119771820a4e2ba7e09735238eb8
Chiyen Venegas <1722025814@qq.com>
2020-05-25 03:44:16 +0200

    added nouns, pronouns, and verb strings

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,41 @@ var sentence = document.getElementById("sentence");
 // var string = sentence.textContent;
 
 completed.addEventListener("click", newString);
+var string = ""
+var nouns = [
+    "house",
+    "movie",
+    "family",
+    "shirt",
+    "hair",
+    "bucket",
+    "cat"
+]
 
+var pronouns = [
+    "my", 
+    "your",
+    "player 1",
+    "player two",
+    "player three",
+    "everyone's"
+]
 
+var verbs = [
+    "brings", 
+    "leaves", 
+    "lends",
+    "loves", 
+    "hates",
+    "fucks"
+]
 function newString() {  
 
 
 //define the string
+
 //define the noun pool
+
 //def. verb pool
 //generate new strings
 //replace the text on the website
